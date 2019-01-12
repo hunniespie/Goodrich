ed3b8fb8dbb35bab172f6fb4ab48355ffd3ad47c
hunniespie <46621971+hunniespie@users.noreply.github.com>
2019-01-12 16:39:43 +0700

    update unique.py
    In line 34 I thought you must change S[j-1] == S[j]
    to temp[j-1] == temp[j]

--- ch03/unique.py
+++ ch03/unique.py
@@ -31,6 +31,6 @@ def unique2(S):
   """Return True if there are no duplicate elements in sequence S."""
   temp = sorted(S)                # create a sorted copy of S
   for j in range(1, len(temp)):
-    if S[j-1] == S[j]:
+    if S[j-1] == S[j]:            """I thought you must change S[j-1] == S[j] to temp[j-1] == temp[j]"""
       return False                # found duplicate pair
   return True                     # if we reach this, elements were unique
